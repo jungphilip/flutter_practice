a4187d05eef91340569935529ff85947e71101c3
Humpty <35593604+jungphilip@users.noreply.github.com>
2023-01-23 18:55:04 +0900

    Create main.dart

---
+++ webtoon/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:toon/screens/home_screen.dart';
+import 'package:toon/services/api_service.dart';
+
+void main() {
+  ApiService().getTodaysToons();
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
