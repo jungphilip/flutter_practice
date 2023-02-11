afa4f3b8bf9885ac44476f83b7a63e12087163c0
Humpty <35593604+jungphilip@users.noreply.github.com>
2023-02-11 15:49:22 +0900

    row column recap (updated 2/11)

--- row_column_prac/main.dart
+++ row_column_prac/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
-import 'screen/home_screen.dart';
+import 'package:row_and_column/screen/home_screen.dart';
+
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: HomeScreen(),
+  runApp(MaterialApp(
+    home: HomeScreen(),
     ),
   );
 }
+
+
---
+++ row_column_prac/screen/home_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          color : Colors.black,
+
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children:[
+                  Container(
+                    color: Colors.red,
+                    width: 50.0,
+                    height: 50.0,
+                  ),
+                  Container(
+                    color: Colors.orange,
+                    width: 50.0,
+                    height: 50.0,
+                  ),
+                  Container(
+                    color: Colors.yellow,
+                    width: 50.0,
+                    height: 50.0,
+                  ),
+                  Container(
+                    color: Colors.green,
+                    width: 50.0,
+                    height: 50.0,
+                  ),
+                ],
+              ),
+              Container(
+                color: Colors.orange,
+                width: 50.0,
+                height: 50.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children:[
+                  Container(
+                    color: Colors.red,
+                    width: 50.0,
+                    height: 50.0,
+                  ),
+                  Container(
+                    color: Colors.orange,
+                    width: 50.0,
+                    height: 50.0,
+                  ),
+                  Container(
+                    color: Colors.yellow,
+                    width: 50.0,
+                    height: 50.0,
+                  ),
+                  Container(
+                    color: Colors.green,
+                    width: 50.0,
+                    height: 50.0,
+                  ),
+                ],
+              ),
+              Container(
+                color: Colors.green,
+                width: 50.0,
+                height: 50.0,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
