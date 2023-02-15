0bc50f9e40303de9b85525b70900d67336c7451d
Humpty <35593604+jungphilip@users.noreply.github.com>
2023-02-15 23:46:15 +0900

    Create main.dart

---
+++ U&I_prac/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:u_and_i/screen/splash_screen.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: SplashScreen(),
+  ));
+}
